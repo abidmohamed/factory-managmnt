1f54b71ac34c4ac1ce0f5cb14d7e16531177143e
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-04-21 14:07:29 +0100

    Service worker offline

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 app_name = 'accounts'
@@ -9,4 +11,8 @@ urlpatterns = [
 
     path('', views.home, name='home'),
 
+    path('serviceworker', (TemplateView.as_view(
+        template_name="serviceworker.js",
+        content_type='application/javascript', )),
+         name='serviceworker'),
 ]
