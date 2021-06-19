7f237031b559576957f99fc34b174dcb2206c45a
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2021-06-19 20:25:09 +0100

    add users and permissions

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
 
     path('', views.home, name='home'),
+    path('add_user/', views.add_user, name='add_user'),
+    path('users_list/', views.users_list, name='users_list'),
+    path('permissions_list/<str:pk>', views.permissions_list, name='permissions_list'),
 
     path('serviceworker', (TemplateView.as_view(
         template_name="serviceworker.js",
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 import collections
 import datetime
+from django.contrib.auth.models import User, Group, Permission
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -7,6 +8,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 # Create your views here.
 from accounts.decorators import admin_only, unauthneticated_user, allowed_user
+from customer.forms import UserForm
 from customer.models import Customer
 from order.models import Order
 from product.models import Product, ProductType
@@ -82,7 +84,7 @@ def home(request):
     for product in productstype:
         # print("TYPES ALERT ------>",StockProduct.objects.all().filter(type=product,
         # quantity__lte=product.alert_quantity))
-        if StockProduct.objects.all().filter(type=product,quantity__lte=product.alert_quantity):
+        if StockProduct.objects.all().filter(type=product, quantity__lte=product.alert_quantity):
             stockproductsalertcount += 1
 
     context = {'customerscount': customerscount,
@@ -94,3 +96,63 @@ def home(request):
                'stockproductsalertcount': stockproductsalertcount,
                }
     return render(request, 'dashboard.html', context)
+
+
+def add_user(request):
+    user_form = UserForm()
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+
+            if Group.objects.all().filter(name='desk_helper'):
+                group = Group.objects.get(name='desk_helper')
+            else:
+                group = Group.objects.create(name='desk_helper')
+
+            user.groups.add(group)
+
+            user.save()
+            # permission = Permission.objects.get(id=1)
+            # print("Permissions ====>", Permission.objects.all())
+            # user.user_permissions.add(5, 7)
+            # print("User Permission ====>", user.user_permissions.all())
+            # print(permission.name)
+
+            return redirect('accounts:permissions_list', user.id)
+        else:
+            return redirect('')
+
+    context = {
+        'user_form': user_form,
+    }
+    return render(request, 'user/add_user.html', context)
+
+
+def users_list(request):
+    users = User.objects.all()
+    context = {
+        "users": users,
+    }
+    return render(request, "user/list_user.html", context)
+
+
+def permissions_list(request, pk):
+    user = User.objects.get(id=pk)
+    permissions = Permission.objects.all()
+    if request.method == 'POST':
+        # print(request.POST.getlist('permission_chosen'))
+        chosen_permissions = request.POST.getlist('permission_chosen')
+        for permission in chosen_permissions:
+            current_permission = Permission.objects.get(id=permission)
+            print(current_permission)
+            user.user_permissions.add(current_permission.id)
+        user.save()
+        return redirect("accounts:users_list")
+    context = {
+        'user': user,
+        'permissions': permissions,
+    }
+
+    return render(request, 'user/permissions_list.html', context)
