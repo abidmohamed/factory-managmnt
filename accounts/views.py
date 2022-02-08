d519c28a194b84d41041de11b20b8335e01333b7
Abid Mohamed Nadhir <abidmohamednadhir@gmail.com>
2022-02-08 10:31:14 +0100

    changes of 07/02/2022

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ import collections
 import datetime
 from django.contrib.auth.models import User, Group, Permission
 
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
